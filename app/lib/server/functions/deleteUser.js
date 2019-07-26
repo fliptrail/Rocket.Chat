363428dd54f7c392992ff1176b76d312a04472bc
Shubham Singh <shubham.singh094@yahoo.com>
2019-07-26 19:08:21 +0530

    Articles: fix some bugs (#69)
    * fix some bugs and improve code structure
    
    * fix undefined issue in ghostAPI
    
    * fix importPackage.js

--- app/lib/server/functions/deleteUser.js
+++ app/lib/server/functions/deleteUser.js
@@ -6,6 +6,7 @@ import { Users, Subscriptions, Messages, Rooms, Integrations, FederationPeers } 
 import { hasRole, getUsersInRole } from '../../../authorization';
 import { settings } from '../../../settings';
 import { Notifications } from '../../../notifications';
+import { callbacks } from '../../../callbacks';
 import { getConfig } from '../../../federation/server/config';
 
 export const deleteUser = function(userId) {
@@ -98,6 +99,7 @@ export const deleteUser = function(userId) {
 	}
 
 	Users.removeById(userId); // Remove user from users database
+	callbacks.run('afterDeleteUser', { _id: userId });
 
 	// Refresh the peers list
 	const { peer: { domain: localPeerDomain } } = getConfig();
